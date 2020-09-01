92cdc62b0bb2ff4dff364265c9c056ed49e04bbd
Alison Bessa <alison.bessa@webjump.com.br>
2020-09-01 14:22:35 -0300

    connect to tmdb api

--- src/main.js
+++ src/main.js
@@ -1,13 +1,41 @@
 import api from './api';
 import './styles.css';
 
+const api_key = '048712d053658b68816866a39f3285b0';
 class App {
   constructor() {
     this.appElement = document.getElementById('app');
     this.appElement.innerHTML = this.render();
   }
+  
+  async getPopularMovies() {
+    try {
+      const response = await api.get(`/3/movie/popular?api_key=${api_key}&language=pt-BR&page=1`);
+      const popularMovies = response.data.results.slice(0, 3);
+      console.log(popularMovies);
+      return popularMovies;
+    }
+    catch {
+      alert('Bad request - Popular Movies');
+    }
+  }
 
+  async getLatestMovies() {
+    try {
+      const latestResponse = await api.get(`/3/movie/now_playing?api_key=${api_key}&language=pt-BR`);
+      const latestMovies = latestResponse.data.results.slice(0, 10);
+      console.log(latestMovies);
+      return latestMovies;
+    }
+    catch {
+      alert('Bad request - Recent Movies');
+    }
+  }
+  
   render() {
+    this.getPopularMovies();
+    this.getLatestMovies();
+
     const app = /*html*/`
       <div class="container">
         <header>
