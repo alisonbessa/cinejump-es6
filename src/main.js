918926d196171720efa556c7fffb35e165829307
Alison Bessa <alison.bessa@webjump.com.br>
2020-09-10 17:16:16 -0300

    Finish popular movies section

--- src/main.js
+++ src/main.js
@@ -4,53 +4,108 @@ import './styles.css';
 const api_key = '048712d053658b68816866a39f3285b0';
 class App {
   constructor() {
+    this.popularMovies = [];
+    
     this.appElement = document.getElementById('app');
-    this.appElement.innerHTML = this.render();
+    this.init();
   }
   
+  async init() {
+    await this.fetchPopularMovies();
+
+    this.appElement.innerHTML = await this.render();
+  }
+
   async getPopularMovies() {
     try {
       const response = await api.get(`/3/movie/popular?api_key=${api_key}&language=pt-BR&page=1`);
-      const popularMovies = response.data.results.slice(0, 3);
-      console.log(popularMovies);
-      return popularMovies;
+      const popularMoviesSliced = response.data.results.slice(0, 3);
+      const movies = this.adjustMoviesData(popularMoviesSliced);
+      return movies;
     }
-    catch {
-      alert('Bad request - Popular Movies');
+    catch(error) {
+      alert('Bad request - Popular Movies', error);
     }
   }
 
-  async getLatestMovies() {
-    try {
-      const latestResponse = await api.get(`/3/movie/now_playing?api_key=${api_key}&language=pt-BR`);
-      const latestMovies = latestResponse.data.results.slice(0, 10);
-      console.log(latestMovies);
-      return latestMovies;
-    }
-    catch {
-      alert('Bad request - Recent Movies');
+  // async getLatestMovies() {
+  //   try {
+  //     const latestResponse = await api.get(`/3/movie/now_playing?api_key=${api_key}&language=pt-BR`);
+  //     let latestMovies = latestResponse.data.results.slice(0, 10);
+  //     console.log(latestMovies);
+  //     return latestMovies;
+  //   }
+  //   catch(error) {
+  //     alert('Bad request - Recent Movies', error);
+  //   }
+  // }
+
+  adjustMoviesData(rawMoviesData) {
+    const movies = rawMoviesData.map((movie) => {
+      return {
+        ...movie,
+        poster_path: `https://image.tmdb.org/t/p/w1280/${movie.poster_path}`,
+        backdrop_path: `https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}`,
+      };
+    });
+    console.log(movies)
+    return movies;
+  }
+
+  async fetchPopularMovies() {
+    if (this.popularMovies.length === 0) {
+      this.popularMovies = await this.getPopularMovies();
     }
   }
-  
-  render() {
-    this.getPopularMovies();
-    this.getLatestMovies();
 
+  render() {    
     const app = /*html*/`
-      <div class="container">
+      <div class='container'>
         <header>
-          <div class='menu-container'>
-            <div>Filmes</div>
-            <div>Séries</div>
-          </div>
-          <div class='title-container'>
-            <img src="./svg/Logo-white.svg" alt="Webjump logo">
-          </div>
-          <div class='icons-container'>
-            <div><img src="./svg/FiSearch.svg" alt="Magnifier icon"></div>
-            <div><img src="./svg/FaRegUserCircle.svg" alt="User icon"></div>
+          <div class='header-content'>
+            <div class='menu-container'>
+              <a href='#'>Filmes</a>
+              <a href='#'>Séries</a>
+            </div>
+            <a href='#' class='title-container'>
+              <img src="./svg/Logo-white.svg" alt="Webjump logo">
+            </a>
+            <div class='icons-container'>
+              <a href='#'>
+                <img src="./svg/FiSearch.svg" alt="Magnifier icon">
+              </a>
+              <a href='#'>
+                <img src="./svg/FaRegUserCircle.svg" alt="User icon">
+              </a>
+            </div>
           </div>
         </header>
+        <section class='popular-movies-section'>
+          <div class='popular-white-bar'></div>
+          <div class='popular-movies-content'>
+            <div class='most-popular-movie'>
+              <img src=${this.popularMovies[0].backdrop_path}>
+              <div class='popular-movie-details'>
+                <h1>${this.popularMovies[0].title}</h1>
+                <p>${this.popularMovies[0].overview.substring(0, 200)}</p>
+              </div>
+            </div>
+            <div class='two-popular-movies-container'>
+              <div class='second-popular-movie'>
+                <img src=${this.popularMovies[1].backdrop_path}>
+                <div class='popular-movie-details'>
+                  <p>${this.popularMovies[1].title}</p>
+                </div>
+              </div>
+              <div class='second-popular-movie'>
+                <img src=${this.popularMovies[2].backdrop_path}>
+                <div class='popular-movie-details'>
+                  <p>${this.popularMovies[2].title}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       <div>
     `;
 
